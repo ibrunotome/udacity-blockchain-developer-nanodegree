52effb65b33a65029bac8387874a0fff5af706ec
Bruno Tomé <ibrunotome@gmail.com>
2018-07-17 13:58:17 -0300

    Building a Simple Private Blockchain Part 6

--- simpleChain.js
+++ simpleChain.js
@@ -1,31 +1,44 @@
-const SHA256 = require('crypto-js/sha256')
+/* ===== SHA256 with Crypto-js ============================|
+|  Learn more: Crypto-js: https://github.com/brix/crypto-js 
+/=========================================================*/
 
-class Block {
-  constructor(data) {
-    this.hash = "",
-    this.height = 0,
-    this.body = data,
-    this.time = 0,
-    this.previousblockhash = ""
-  }
+const SHA256 = require('crypto-js/sha256');
+
+/* ===== Block Class ==============================
+|  Class with a constructor for block 			   |
+|  ===============================================*/
+
+class Block{
+	constructor(data){
+     this.hash = "",
+     this.height = 0,
+     this.body = data,
+     this.time = 0,
+     this.previousBlockHash = ""
+    }
 }
 
-class Blockchain {
-  constructor() {
+/* ===== Blockchain Class ==========================
+|  Class with a constructor for new blockchain 		|
+|  ================================================*/
+class Blockchain{
+  constructor(){
     this.chain = [];
-    this.addBlock(new Block("First block in the chain - Genesis block"))
+    this.addBlock(new Block("First block in the chain - Genesis block"));
   }
-  
-  addBlock(newBlock) {
-    newBlock.height = this.chain.length
-    newBlock.time = new Date().getTime().toString().slice(0,-3)
-    
-    if (this.chain.length > 0) {
-      newBlock.previousblockhash = this.chain[this.chain.length-1].hash
+  addBlock(newBlock){
+    // Block height
+    newBlock.height = this.chain.length;
+    // UTC timestamp
+    newBlock.time = new Date().getTime().toString().slice(0,-3);
+    // previous block hash
+    if(this.chain.length>0){
+      newBlock.previousBlockHash = this.chain[this.chain.length-1].hash;
     }
-    
-    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString()
-    this.chain.push(newBlock);
+    // Block hash with SHA256 using newBlock and converting to a string
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString();
+    // Adding block object to chain
+  	this.chain.push(newBlock);
   }
 }
 
