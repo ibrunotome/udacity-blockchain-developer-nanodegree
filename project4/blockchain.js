b81aead7938671d1346e2ce537f53ae7c9153428
Bruno Tomé <ibrunotome@gmail.com>
2018-09-07 09:36:40 -0300

    Start project4

---
+++ project4/blockchain.js
@@ -0,0 +1,160 @@
+const SHA256 = require('crypto-js/sha256')
+const Block = require('./block')
+
+/**
+ * Criteria: Configure simpleChain.js with levelDB to persist blockchain dataset using the level Node.js library.
+ */
+const level = require('level')
+const chainDB = './chaindata'
+const db = level(chainDB)
+
+class Blockchain {
+  constructor() {
+    /**
+     * Criteria: Genesis block persist as the first block in the blockchain using LevelDB.
+     */
+    this.getBlockHeight().then((height) => {
+      if (height === -1) {
+        this.addBlock(new Block("Genesis block")).then(() => console.log("Genesis block added!"))
+      }
+    })
+  }
+
+  /**
+   * Criteria: addBlock(newBlock) function includes a method to store newBlock with LevelDB.
+   * 
+   * @param {Block} newBlock 
+   */
+  async addBlock(newBlock) {
+    const height = parseInt(await this.getBlockHeight())
+
+    newBlock.height = height + 1
+    newBlock.time = new Date().getTime().toString().slice(0, -3)
+
+    if (newBlock.height > 0) {
+      const prevBlock = await this.getBlock(height)
+      newBlock.previousBlockHash = prevBlock.hash
+      console.log(`Previous hash: ${newBlock.previousBlockHash}`)
+    }
+
+    newBlock.hash = SHA256(JSON.stringify(newBlock)).toString()
+    console.log(`New hash: ${newBlock.hash}`)
+
+    await this.addBlockToDB(newBlock.height, JSON.stringify(newBlock))
+  }
+
+  /**
+   * Criteria: Modify getBlockHeight() function to retrieve current block height within the LevelDB chain.
+   */
+  async getBlockHeight() {
+    return await this.getBlockHeightFromDB()
+  }
+
+  /**
+   * Criteria: Modify getBlock() function to retrieve a block by it's block heigh within the LevelDB chain.
+   * 
+   * @param {int} blockHeight 
+   */
+  async getBlock(blockHeight) {
+    return JSON.parse(await this.getBlockFromDB(blockHeight))
+  }
+
+  /**
+   * Criteria: Modify the validateBlock() function to validate a block stored within levelDB.
+   * 
+   * @param {int} blockHeight 
+   */
+  async validateBlock(blockHeight) {
+    let block = await this.getBlock(blockHeight);
+    let blockHash = block.hash;
+    block.hash = '';
+    
+    let validBlockHash = SHA256(JSON.stringify(block)).toString();
+
+    if (blockHash === validBlockHash) {
+        return true;
+      } else {
+        console.log(`Block #${blockHeight} invalid hash: ${blockHash} <> ${validBlockHash}`);
+        return false;
+      }
+  }
+
+  /**
+   * Criteria: Modify the validateChain() function to validate blockchain stored within levelDB.
+   */
+  async validateChain() {
+    let errorLog = []
+    let previousHash = ''
+    let isValidBlock = false
+
+    const heigh = await this.getBlockHeightFromDB()
+
+    for (let i = 0; i < heigh; i++) {
+      this.getBlock(i).then((block) => {
+        isValidBlock = this.validateBlock(block.height)
+
+        if (!isValidBlock) {
+          errorLog.push(i)
+        } 
+
+        if (block.previousBlockHash !== previousHash) {
+          errorLog.push(i)
+        }
+
+        previousHash = block.hash
+
+        if (i === (heigh -1)) {
+          if (errorLog.length > 0) {
+            console.log(`Block errors = ${errorLog.length}`)
+            console.log(`Blocks: ${errorLog}`)
+          } else {
+            console.log('No errors detected')
+          }
+        }
+      })
+    }
+  }
+
+  // Level db functions
+
+  addBlockToDB(key, value) {
+    return new Promise((resolve, reject) => {
+      db.put(key, value, (error) => {
+        if (error) {
+          reject(error)
+        }
+
+        console.log(`Added block #${key}`)
+        resolve(`Added block #${key}`)
+      })
+    })
+  }
+
+  getBlockFromDB(key) {
+    return new Promise((resolve, reject) => {
+      db.get(key, (error, value) => {
+        if (error) {
+          reject(error)
+        }
+
+        resolve(value)
+      })
+    })
+  }
+
+  getBlockHeightFromDB() {
+    return new Promise((resolve, reject) => {
+      let height = -1
+
+      db.createReadStream().on('data', (data) => {
+        height++
+      }).on('error', (error) => {
+        reject(error)
+      }).on('close', () => {
+        resolve(height)
+      })
+    })
+  }
+}
+
+module.exports = Blockchain
---
+++ project4/index.js
@@ -0,0 +1,49 @@
+/**
+ * Criteria: Configure private blockchain project to include a RESTful API with Node.js framework running on port 8000.
+ */
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser')
+const Block = require('./block')
+const Blockchain = require('./blockchain')
+const chain = new Blockchain()
+
+app.listen(8000, () => console.log('API listening on port 8000'))
+app.use(bodyParser.json())
+app.get('/', (req, res) => res.status(404).json({
+  "status": 404,
+  "message": "Accepted endpoints: POST /block or GET /block/{BLOCK_HEIGHT}"
+}))
+
+/**
+ * Criteria: GET Block endpoint using URL path with block height parameter. Preferred URL path http://localhost:8000/block/{BLOCK_HEIGHT}
+ */
+app.get('/block/:height', async (req, res) => {
+  try {
+    const response = await chain.getBlock(req.params.height)
+    res.send(response)
+  } catch (error) {
+    res.status(404).json({
+      "status": 404,
+      "message": "Block not found"
+    })
+  }
+})
+
+/**
+ * Criteria: POST Block endpoint using key/value pair within request body. Preferred URL path http://localhost:8000/block
+ */
+app.post('/block', async (req, res) => {
+  if (req.body.body === '' || req.body.body === undefined) {
+    res.status(400).json({
+      "status": 400,
+      message: "Fill the body parameter"
+    })
+  }
+
+  await chain.addBlock(new Block(req.body.body))
+  const height = await chain.getBlockHeight()
+  const response = await chain.getBlock(height)
+
+  res.send(response)
+})
