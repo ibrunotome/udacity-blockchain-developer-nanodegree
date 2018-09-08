f59db2598a7bdd9a127027e7fc7f3185fe2f707a
Bruno Tomé <ibrunotome@gmail.com>
2018-09-08 15:00:32 -0300

    Partially implement the Star registration Endpoint criteria

--- project4/index.js
+++ project4/index.js
@@ -23,6 +23,13 @@ app.get('/', (req, res) => res.status(404).json({
  * Criteria: Web API post endpoint validates request with JSON response.
  */
 app.post('/requestValidation', async (req, res) => {
+  if (!req.body.address) {
+    res.status(400).json({
+      "status": 400,
+      message: "Fill the address parameter"
+    })
+  }
+
   const address = req.body.address
   const timestamp = Date.now()
 
@@ -45,6 +52,13 @@ app.post('/requestValidation', async (req, res) => {
  * Criteria: Web API post endpoint validates message signature with JSON response.
  */
 app.post('/message-signature/validate', async (req, res) => {
+  if (!req.body.address || !req.body.signature) {
+    res.status(400).json({
+      "status": 400,
+      message: "Fill the address and signature parameters"
+    })
+  }
+
   const { address, signature } = req.body
 
   db.get(address)
@@ -92,14 +106,45 @@ app.get('/block/:height', async (req, res) => {
  * Criteria: POST Block endpoint using key/value pair within request body. Preferred URL path http://localhost:8000/block
  */
 app.post('/block', async (req, res) => {
-  if (req.body.body === '' || req.body.body === undefined) {
+  if (!req.body.address || !req.body.star) {
     res.status(400).json({
       "status": 400,
-      message: "Fill the body parameter"
+      message: "Fill the address and star parameters"
     })
   }
 
-  await chain.addBlock(new Block(req.body.body))
+  const body = { address, star } = req.body
+  const { dec, ra, story } = star
+
+  // Validate ra, dec, story 
+  if (typeof dec !== 'string' || typeof ra !== 'string' || typeof story !== 'string' || !dec.length || !ra.length || !story.length) {
+    return res.status(400).json({
+        reason: 'Bad request',
+        details: "Your star information should include non-empty string properties 'dec', 'ra' and 'story'"
+    })
+  }
+
+  // Validate if story length less than 500 bytes
+  if (new Buffer(story).length > 500) {
+    return res.status(400).json({
+        reason: 'Bad request',
+        details: 'Your star story too is long. Maximum size is 500 bytes'
+    })
+  }
+
+  // Check if string contains only ASCII symbols (0-126 char codes)
+  const isASCII = ((str) =>  /^[\x00-\x7F]*$/.test(str))
+
+  if (!isASCII(story)) {
+    return res.status(400).json({
+        reason: 'Bad request',
+        details: 'Your star story contains non-ASCII symbols'
+    })
+  }
+
+  body.star.story = new Buffer(story).toString('hex')
+
+  await chain.addBlock(new Block(body))
   const height = await chain.getBlockHeight()
   const response = await chain.getBlock(height)
 
